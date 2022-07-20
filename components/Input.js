f09534ac6647b55254ea91189a4a8c748a019d32
Thiago Sullivan <thiagosullivan12@gmail.com>
2022-07-20 04:39:28 -0300

    Creating input of feed

--- components/Input.js
+++ components/Input.js
@@ -1,62 +1,86 @@
 import React, { useEffect, useRef, useState } from 'react';
+import { db, storage } from "../firebase";
+import {
+  addDoc,
+  collection,
+  doc,
+  serverTimestamp,
+  updateDoc,
+} from "@firebase/firestore";
+import { getDownloadURL, ref, uploadString } from "@firebase/storage";
 import { CalendarIcon, ChartBarIcon, EmojiHappyIcon, PhotographIcon } from '@heroicons/react/outline';
 import { XIcon } from '@heroicons/react/solid';
 import data from '@emoji-mart/data'
+import { Picker } from "emoji-mart";
+import "emoji-mart/css/emoji-mart.css";
+
 
 
 function Input() {
     const [input, setInput] = useState('');
     const [selectedFile, setSelectedFile] = useState(null);
     const [showEmojis, setShowEmojis] = useState(false);
-    const filePickerRef = useRef(null)
+    const filePickerRef = useRef(null);
+    const [loading, setLoading] = useState(false);
 
-    const addImageToPost = () => {
+    const sendPost = async () => {
+        if(loading) return;
+        setLoading(true);
 
-    }
+        const docRef = await addDoc(collection(db, 'posts'), {
+            // id: session.user.uid,
+            // username: session.user.name,
+            // userImg: session.user.image,
+            // tag: session.user.tag,
+            text: input,
+            timestamp: serverTimestamp(),
+        })
 
-    const EmojiPicker = (props) => {
-        const ref = useRef();
-        const showEmojis = useRef(true);
-      
-        useEffect(() => {
-          if (showEmojis.current) {
-            showEmojis.current = false;
-            import("emoji-mart").then((EmojiMart) => {
-              new EmojiMart.Picker({ ...props, data, ref });
+        const imageRef = ref(storage, `posts/${docRef.id}/image`);
+
+        if(selectedFile){
+            await uploadString(imageRef, selectedFile, "data_url").then(async () => {
+                const downloadURL = await getDownloadURL(imageRef)
+                await updateDoc(doc(db, "posts", docRef.id), {
+                    image: downloadURL,
+                });
             });
-          }
-        }, [props]);
-      
-        return <div
-                    ref={ref}
-                    style={{
-                        position: "absolute",
-                        marginTop: "465px",
-                        marginLeft: -40,
-                        maxWidth: "320px",
-                        borderRadius: "20px",
-                    }}
-                    onSelect={addEmoji}
-                ></div>;
-      };
+        }
 
-      const addEmoji = (e) => {
+        setLoading(false);
+        setInput("");
+        setSelectedFile(null);
+        setShowEmojis(false);
+    };
+
+    const addImageToPost = (e) => {
+        const reader = new FileReader();
+        if (e.target.files[0]) {
+            reader.readAsDataURL(e.target.files[0]);
+        }
+
+        reader.onload = (readerEvent) => {
+            setSelectedFile(readerEvent.target.result);
+        };
+    };
+
+    const addEmoji = (e) => {
         let sym = e.unified.split("-");
         let codesArray = [];
         sym.forEach((el) => codesArray.push("0x" + el));
         let emoji = String.fromCodePoint(...codesArray);
         setInput(input + emoji);
-      };
+    };
 
   return (
-    <div className={`border-b border-gray-700 p-3 flex space-x-3 `}>
+    <div className={`border-b border-gray-700 p-3 flex space-x-3 ${loading && "opacity-60"}`}>
         <img
             src='https://media.istockphoto.com/vectors/letter-t-logo-lettermark-monogram-typeface-type-emblem-character-vector-id1279365501?k=20&m=1279365501&s=612x612&w=0&h=1UFI8mDtVEZR82_KTCN6qFsCa824X7-Wmg1dz0vBERE='
             alt=''
             className='h-11 w-11 rounded-full cursor-pointer'
         />
         <div className='w-full divide-y divide-gray-700'>
-            <div className={``}>
+            <div className={`${selectedFile && "pb-7"} ${input && "space-y-2.5"}`}>
                 <textarea
                     value={input}
                     onChange={(e) => setInput(e.target.value)}
@@ -66,18 +90,23 @@ function Input() {
                 />
                 
                 {selectedFile && (
-                    <div className='relative'>
-                        <div
-                            className='absolute w-8 h-8 bg-[#15181c] hover:bg-[#272c26] bg-opacity-75 rounded-full flex items-center justify-center top-1 left-1 cursor-pointer'
-                            onClick={() => setSelectedFile(null)}
-                        >
-                            <XIcon className='text-white h-5' />
-                        </div>
-                        <img src={selectedFile} alt="" className='rounded-2xl max-h-80 object-contain'/>
+                    <div className="relative">
+                    <div
+                        className="absolute w-8 h-8 bg-[#15181c] hover:bg-[#272c26] bg-opacity-75 rounded-full flex items-center justify-center top-1 left-1 cursor-pointer"
+                        onClick={() => setSelectedFile(null)}
+                    >
+                        <XIcon className="text-white h-5" />
+                    </div>
+                    <img
+                        src={selectedFile}
+                        alt=""
+                        className="rounded-2xl max-h-80 object-contain"
+                    />
                     </div>
                 )}
             </div>
-            <div className='flex items-center justify-between pt-2.5'>
+            {!loading && (
+                <div className='flex items-center justify-between pt-2.5'>
                     <div className='flex items-center'>
                         <div className='icon' onClick={() => filePickerRef.current.click()}>
                             <PhotographIcon className='h-[22px] text-[#1d9bf0]' />
@@ -94,20 +123,28 @@ function Input() {
                         </div>
 
                         {showEmojis && (
-                            <EmojiPicker
+                            <Picker
                                 onSelect={addEmoji}
                                 style={{
-                                    position: "absolute",
-                                    marginTop: "465px",
-                                    marginLeft: -40,
-                                    maxWidth: "320px",
-                                    borderRadius: "20px",
+                                position: "absolute",
+                                marginTop: "465px",
+                                marginLeft: -40,
+                                maxWidth: "320px",
+                                borderRadius: "20px",
                                 }}
                                 theme="dark"
-                          />
+                        />
                         )}
                     </div>
-            </div>
+                    <button
+                        className='bg-[#1d9bf0] text-white rounded-full px-4 py-1.5 font-bold shadow-md hover:bg-[#1a8cd8] disabled:hover:bg-[#1d9bf0] disabled:opacity-50 disabled:cursor-default'
+                        disabled={!input.trim() && !selectedFile}
+                        onClick={sendPost}
+                    >
+                        Tweet
+                    </button>
+                </div>
+            )}
         </div>
     </div>
   )
